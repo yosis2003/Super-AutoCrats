48d6beb737f2d7df1bd998277f293c1cdb2a24da
PallyBae <Matthewdouglascraig@gmail.com>
2023-06-01 11:16:15 -0700

    Displaying Leaders
    Got the leader to display from the complete list in a shortcut way. And I added comments to help guide and remind us to change this to be done in a shop display function of sorts.

--- Assets/ShopScript.cs
+++ Assets/ShopScript.cs
@@ -3,6 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+public class Leader
+{
+    const float LEGAL_MAX = 50;
+    float initialHealth;
+    float currHealth;
+    float initialDamage;
+    float currDamage;
+    string name;
+    GameObject leaderRepresentative;
+
+    public Leader(string leaderType, GameObject leaderObject, float initH, float initD)
+    {
+        name = leaderType;
+        leaderRepresentative = leaderObject;
+        initialHealth = initH;
+        initialDamage = initD;
+        currHealth = initialHealth;
+        currDamage = initialDamage;
+    }
+    public void SetSpriteVisible(bool visible)
+    {
+        SpriteRenderer spriteRenderer = leaderRepresentative.GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.enabled = visible;
+
+            //Realistically this should not be part of things, this is initializing
+            //the position of the sprite and is a quick fix that should be replaced
+            //by a function that does this independantly of displaying the sprite.
+            leaderRepresentative.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+    }
+}
 
 public class ShopScript : MonoBehaviour
 {
@@ -15,8 +48,15 @@ public class ShopScript : MonoBehaviour
 
     void Start()
     {
-        Obama = new GameObject("Obama");
+        Obama = GameObject.Find("Obama");
         Obamna = new Leader("Obama", Obama, 10, 7);
+
+        completeList.Add(Obamna);
+
+        //Technically we dont want to be showing things
+        //from the complete list, we will eventually replace
+        //this with shop however for now we are working with complete list
+        completeList[0].SetSpriteVisible(true);
     }
 
     void petLoader()
